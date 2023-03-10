d7badcf9be7be72529da80c88d812cd2b766d729
evgenii klimashko <klimashko108@gmail.com>
2023-03-10 15:32:37 +0300

    Добавил вариант решения 3 задачи  test_readable_function(), этот вариант без функции, тк пока не нашел способ извлечь значение параметра переданной ф-ии

--- test_python_basics_2.py
+++ test_python_basics_2.py
@@ -1,4 +1,6 @@
 from datetime import time
+#import inspect
+
 
 def test_dark_theme():
     """
@@ -59,18 +61,25 @@ def test_find_suitable_user():
 # >>> open_browser(browser_name="Chrome")
 # "Open Browser [Chrome]"
 
+#def change_name_function(func, test_readable_function):
+    #return f'{func.__name__.replace("_", " ").title()} {[inspect.signature(func).parameters.values()]}'
+
 def test_readable_function():
     open_browser(browser_name="Chrome")
     go_to_companyname_homepage(page_url="https://companyname.com")
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
+
 def open_browser(browser_name):
-    actual_result = None
+    #actual_result = change_name_function(open_browser, test_readable_function)
+    actual_result = f'{open_browser.__name__.replace("_", " ").title()} [{browser_name}]'
     assert actual_result == "Open Browser [Chrome]"
 
 def go_to_companyname_homepage(page_url):
     actual_result = None
+    actual_result = f'{go_to_companyname_homepage.__name__.replace("_", " ").title()} [{page_url}]'
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 def find_registration_button_on_login_page(page_url, button_text):
     actual_result = None
+    actual_result = f'{find_registration_button_on_login_page.__name__.replace("_", " ").title()} [{page_url}, {button_text}]'
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
